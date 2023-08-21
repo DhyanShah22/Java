6c3848bbb8e0a6e0c2547e24581e92c1bca55675
DhyanShah22 <dhyanshah229@gmail.com>
2023-08-21 15:17:30 +0530

    Lab

--- Practical-2/Digit.java
+++ Practical-2/Digit.java
@@ -2,13 +2,13 @@ public class Digit
 {
 public static void main(String args[])
 {
-int number=632;
+int number=6574;
 int sum=0;
 System.out.println("the number is "+number);
 
 while(number > 0)
 {
-
+       
 int digit = number % 10;
 
 sum = sum + digit;
--- Practical-2/pyramid.java
+++ Practical-2/pyramid.java
@@ -1,4 +1,4 @@
-public class pyst {
+public class pyramid {
         public static void main(String[] args) {
             String input = "stream";
 
